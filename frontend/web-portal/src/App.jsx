d1195bc43a483e0c10bb0d71cdf3ba18ad18278a
sachiniariyarathne <140165671+sachiniariyarathne@users.noreply.github.com>
2025-08-27 18:48:47 +0530

    components moved to pages folder

--- frontend/web-portal/src/App.jsx
+++ frontend/web-portal/src/App.jsx
@@ -1,21 +1,22 @@
 
 import { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
 import TopNavbar from './pages/TopNavbar.jsx';
+import Footer from './pages/Footer.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <TopNavbar />
       <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
-
 export default App;
