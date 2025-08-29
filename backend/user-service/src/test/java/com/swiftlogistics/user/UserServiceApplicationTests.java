0cb15223ab7eebb08f8dbc3bd434bade8a99bd20
Okadini <okadinikdi@gmail.com>
2025-08-29 10:26:19 +0530

    error fixed

---
+++ backend/user-service/src/test/java/com/swiftlogistics/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swiftlogistics.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = UserServiceApplication.class)
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
